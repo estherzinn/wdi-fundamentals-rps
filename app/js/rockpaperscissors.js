a535991b56278f75d8777e7e4cfc570d4e124c76
Esther Zinn <Bionicfemme@me.com>
2015-07-28 14:54:53 -0400

    Wrote conditions for winning

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -1,12 +1,12 @@
 ////////////////////////////////////////////////
 /*   Provided Code - Please Don't Edit   */
 ////////////////////////////////////////////////
-'use strict';
 
 function getInput() {
     console.log("Please choose either 'rock', 'paper', or 'scissors'.")
     return prompt();
 }
+
 function randomPlay() {
     var randomNumber = Math.random();
     if (randomNumber < 0.33) {
@@ -30,12 +30,32 @@ function getComputerMove(move) {
 }
 
 function getWinner(playerMove,computerMove) {
-    var winner;
-    // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
-    // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
-    // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
-    return winner;
+  function getWinner(playerMove,computerMove) {
+      var winner;
+      if (playerMove === computerMove) {
+          return 'tie';
+      }
+      else if (playerMove === 'rock') {
+          if(computerMove === 'scissors') {
+              return 'player';
+          }
+          else {
+              return 'computer';
+          }}
+      else if (playerMove === 'paper') {
+          if(computerMove === 'rock') {
+              return 'player';
+          }
+          else {
+              return 'computer';
+          }}
+      else if (playerMove === 'scissors') {
+          if(computerMove === 'paper') {
+              return 'player';
+          }
+          else {
+              return 'computer';
+          }}}
 }
 
 function playToFive() {
